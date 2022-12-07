29ecd75993e5679c9f28f67e3cc9d110712e34ef
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-07 20:26:35 +0000

    Adding tarfile member sanitization to extractall()

--- fl_pytorch/data_preprocess/h5_tff_dataset.py
+++ fl_pytorch/data_preprocess/h5_tff_dataset.py
@@ -42,7 +42,26 @@ class H5TFFDataset(FLDataset):
             def extract_bz2(filename, path="."):
                 import tarfile
                 with tarfile.open(filename, "r:bz2") as tar:
-                    tar.extractall(path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path)
             taret_file = os.path.join(one_up, target)
             if os.path.isfile(taret_file):
                 extract_bz2(os.path.join(one_up, target), one_up)
